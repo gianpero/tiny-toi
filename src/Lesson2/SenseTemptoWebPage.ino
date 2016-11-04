d1030b55db08837a1a96fbc479a75421ccf13837
gianpero <gianpero@gmail.com>
2016-11-04 15:50:17 -0500

    Change the declaration of variables on the file

--- src/Lesson2/SenseTemptoWebPage.ino
+++ src/Lesson2/SenseTemptoWebPage.ino
@@ -1,50 +1,22 @@
-/*
-  Web Server
-
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
-
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 1, 177);
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
 EthernetServer server(80);
+int tempRaw;
+float temp;
+int B=4250;
+float equivResistance;
 
 void setup() {
-  int tempRaw;
-  float temp;
-  int B=4250;
-  float equivResistance;
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ;
   }
 
 
-  // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.print("server is at ");
@@ -80,7 +52,7 @@ void loop() {
           tempRaw=analogRead(0);
           equivResistance=(float)(1023-tempRaw)*10000/tempRaw;
           temp=1/(log(equivResistance/10000)/B+1/298.15)-273.15;
-          client.println("Temperature is: ");
+          client.println("Temperature is ");
           client.println(temp);
           client.println("</html>");
           break;
@@ -96,7 +68,6 @@ void loop() {
     }
     // give the web browser time to receive the data
     delay(1);
-    // close the connection:
     client.stop();
     Serial.println("client disconnected");
   }
